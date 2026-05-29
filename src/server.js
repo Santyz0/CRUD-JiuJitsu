aed2e522ce8af3fa2edef60e2ac3d05d26b5a841
Santyz0 <sandro.rob2005@gmail.com>
2026-05-29 16:16:51 -0300

    feat: Servidor express configurado

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { routes } from './routes';
+
+const app = express();
+
+// Informamos ao Express que vamos trabalhar com requisições no formato JSON
+app.use(express.json());
+
+// Injetamos as nossas rotas no aplicativo
+app.use(routes);
+
+// Iniciamos o servidor na porta 3333
+app.listen(3333, () => {
+  console.log('🚀 Servidor da Academia rodando na porta 3333!');
+});
